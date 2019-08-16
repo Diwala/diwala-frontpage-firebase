0fc52d0cb9a85689669cfa54d29d9c9291979a95
Snorre lothar von Gohren Edwin <snorre.edwin@gmail.com>
2019-08-16 09:39:21 +0200

    Fix: url for slack

--- src/components/slack_button/SlackButton.tsx
+++ src/components/slack_button/SlackButton.tsx
@@ -4,7 +4,7 @@ import LinkButton from '../buttons/link/LinkButton';
 class SlackButton extends React.Component {
   public render() {
     const joinSlack = 'https://join.slack.com/t/';
-    const slackUrl = joinSlack + 'diwala-org/shared_invite/enQtMzM2MDY1NjAzMTU2LTk1NTI0OThkNWZkOTQ2MjI5ZGEwMTgyOWVhZDA5ZDM0NzA1ZDA1MjI2YTJhZWU2YTM0NGUxNTI5ZGMyNDVlMmQ';
+    const slackUrl = joinSlack + 'diwala/shared_invite/enQtMzM2MDY1NjAzMTU2LTg2ZmU1YjZiZWNiYzFmNTU4M2M2ZDBiMjk3MGQ1MzBjOWIzNjViY2E5Y2U4NTc3ODEzZGJhYjIzYjY5OWRmZmI';
     return (
       <LinkButton
         classes="button button--join"
